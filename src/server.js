1e35f3fd0e6e10cba06a2629fb900107e0012f79
Ign1s123 <genamisinskij@gmail.com>
2026-09-14 22:41:54 +0300

    feat: configure CORS to allow requests from localhost:3000

--- src/server.js
+++ src/server.js
@@ -16,7 +16,12 @@ import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000', 
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(logger);
 app.use(cookieParser());
