513bfda02b1df87a2ce01e3bfb55191787217560
Ign1s123 <genamisinskij@gmail.com>
2026-09-24 23:10:06 +0300

    feat: update CORS configuration to allow requests from Vercel deployment

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000', 
+    origin: ['http://localhost:3000', 'https://fonik-inky.vercel.app'],
     credentials: true,
   })
 );
